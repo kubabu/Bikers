af0f28a21bc1630385869eb4abf29902ea0499e5
Waldemar Panas <waldemarpanas@hotmail.com>
2015-12-05 18:03:28 +0100

    overall changes

--- static/src/js/controllers/src/nav.js
+++ static/src/js/controllers/src/nav.js
@@ -2,6 +2,7 @@ angular.module('controllers.nav', []).controller('NavCtrl', ['$scope', function 
     $scope.urls = [
         {path: '#/bikes/', name: 'Rowery', active: false},
         {path: '#/routes/', name: 'Trasy', active: false},
-        {path: '#/users/', name: 'Tablica', active: false}
+        {path: '#/users/', name: 'Tablica', active: false},
+        {path: '#/messages/', name: 'Wiadomości', active: false}
     ];
 }]);
