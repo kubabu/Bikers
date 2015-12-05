4b184971fe5f7951f7d91a5cb6d9585d5d48b978
Waldemar Panas <waldemarpanas@hotmail.com>
2015-12-05 18:09:26 +0100

    small changes and bullshit

--- static/src/js/controllers/src/nav.js
+++ static/src/js/controllers/src/nav.js
@@ -1,8 +1,8 @@
 angular.module('controllers.nav', []).controller('NavCtrl', ['$scope', function ($scope) {
     $scope.urls = [
+        {path: '#/users/', name: 'Tablica', active: false},
         {path: '#/bikes/', name: 'Rowery', active: false},
         {path: '#/routes/', name: 'Trasy', active: false},
-        {path: '#/users/', name: 'Tablica', active: false},
         {path: '#/messages/', name: 'Wiadomości', active: false}
     ];
 }]);
