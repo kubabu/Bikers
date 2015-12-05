0c59510afc21f7670789fc6b5dfacd3b8f29eba3
Waldemar Panas <waldemarpanas@hotmail.com>
2015-12-05 16:43:10 +0100

    controllers.routes fix

--- static/src/js/controllers/src/route.js
+++
@@ -1,7 +0,0 @@
-angular.module('controllers.route', []).controller('RouteNewCtrl', ['$scope', function ($scope) {
-
-}]);
-
-angular.module('controllers.route').controller('RouteShowCtrl', ['$scope', function ($scope) {
-
-}]);
---
+++ static/src/js/controllers/src/routes.js
@@ -0,0 +1,7 @@
+angular.module('controllers.routes', []).controller('RouteNewCtrl', ['$scope', function ($scope) {
+
+}]);
+
+angular.module('controllers.routes').controller('RouteShowCtrl', ['$scope', function ($scope) {
+
+}]);
