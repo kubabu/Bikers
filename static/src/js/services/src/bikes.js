3e08ad402e4076a98e012f59cfa1a2de4a5cb5ba
Waldemar Panas <waldemarpanas@hotmail.com>
2015-12-05 18:29:55 +0100

    add services bikes

---
+++ static/src/js/services/src/bikes.js
@@ -0,0 +1,61 @@
+angular.module('services.bikes').service('BikesSvc', ['$http', '$q', 'url', function ($http, $q, url) {
+    var self = {};
+
+    self.getBikes = function(filters) {
+        var defer = $q.defer();
+
+        $http.get(url + 'bikes/', angular.extend({}, filters)).then(function (res) {
+            if (res.data.status) {
+                defer.resolve(res.data.results);
+            } else {
+                defer.resolve([]);
+            }
+        });
+
+        return defer.promise;
+    };
+
+    self.addBike = function (bike) {
+        var defer = $q.defer();
+
+        $http.post(url + 'bikes/', {data: [bike]}).then(function (res) {
+            if (res.data.status) {
+                defer.resolve(res.data.results);
+            } else {
+                defer.resolve([]);
+            }
+        });
+
+        return defer.promise;
+    };
+
+    self.editBike = function (bike) {
+        var defer = $q.defer();
+
+        $http.put(url + 'bikes/', {data: [bike]}).then(function (res) {
+            if (res.data.status) {
+                defer.resolve(res.data.results);
+            } else {
+                defer.resolve([]);
+            }
+        });
+
+        return defer.promise;
+    };
+
+    self.deleteBike = function (id) {
+        var defer = $q.defer();
+
+        $http.put(url + 'bikes/', {data: [id]}).then(function (res) {
+            if (res.data.status) {
+                defer.resolve(res.data.results);
+            } else {
+                defer.resolve([]);
+            }
+        });
+
+        return defer.promise;
+    };
+
+    return self;
+}]);
