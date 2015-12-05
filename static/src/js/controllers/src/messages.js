05c25793b2369b0b4acb7c6d92810213ca59d303
Waldemar Panas <waldemarpanas@hotmail.com>
2015-12-05 18:18:39 +0100

    templates messages/new

--- static/src/js/controllers/src/messages.js
+++ static/src/js/controllers/src/messages.js
@@ -1,3 +1,7 @@
 angular.module('controllers.messages', []).controller('MessagesCtrl', ['$scope', function ($scope) {
     $scope.messages = [];
 }]);
+
+angular.module('controllers.messages').controller('MessagesNewCtrl', ['$scope', function ($scope) {
+
+}]);
