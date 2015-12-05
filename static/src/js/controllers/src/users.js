c78d173cb29c7c99c006e7bc5a2a1842961e1618
Kuba Buda <kubabuda@gazeta.pl>
2015-12-05 18:05:37 +0100

    show users routes in /users/main template

--- static/src/js/controllers/src/users.js
+++ static/src/js/controllers/src/users.js
@@ -1,6 +1,9 @@
 angular.module('controllers.users', []).controller('UsersCtrl', ['$scope', function ($scope) {
     $scope.user = {
-        bikes: []
+        bikes: [{name:"Romet Osa", description:"Składak z koralikami na kołach"}],
+        routes: [{name:"do sklepu", bike:{name:"Romet"},
+                 date_of_ride:"2015-12-04 16:45", duration_of_ride:12
+        }]
     };
 }]);
 
