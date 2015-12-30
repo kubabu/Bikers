6eed330a6d6fa2e33366a30331159176bd417668
Waldemar Panas <waldemarpanas@hotmail.com>
2015-12-30 20:16:36 +0100

    #3

---
+++ static/src/js/services/src/parts.js
@@ -0,0 +1,61 @@
+angular.module('services.parts', []).service('PartsSvc', ['$http', '$q', 'url', function ($http, $q, url) {
+    var self = {};
+
+    self.getParts = function(filters) {
+        var defer = $q.defer();
+
+        $http.get(url + 'parts/', angular.extend({}, filters)).then(function (res) {
+            if (res.data.status) {
+                defer.resolve(res.data.results);
+            } else {
+                defer.resolve([]);
+            }
+        });
+
+        return defer.promise;
+    };
+
+    self.addPart = function (part) {
+        var defer = $q.defer();
+
+        $http.post(url + 'parts/', {data: [part]}).then(function (res) {
+            if (res.data.status) {
+                defer.resolve(res.data.results);
+            } else {
+                defer.resolve([]);
+            }
+        });
+
+        return defer.promise;
+    };
+
+    self.editPart = function (part) {
+        var defer = $q.defer();
+
+        $http.put(url + 'parts/', {data: [part]}).then(function (res) {
+            if (res.data.status) {
+                defer.resolve(res.data.results);
+            } else {
+                defer.resolve([]);
+            }
+        });
+
+        return defer.promise;
+    };
+
+    self.deletePart = function (id) {
+        var defer = $q.defer();
+
+        $http.put(url + 'parts/', {data: [id]}).then(function (res) {
+            if (res.data.status) {
+                defer.resolve(res.data.results);
+            } else {
+                defer.resolve([]);
+            }
+        });
+
+        return defer.promise;
+    };
+
+    return self;
+}]);
