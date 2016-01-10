bd1b3de9964e904ec222309e3f7225391707541f
Waldemar Panas <waldemarpanas@hotmail.com>
2016-01-10 09:39:07 +0100

    #7

---
+++ api/src/Modules/Routes/Landmarks.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: waldek
+ * Date: 10.01.16
+ * Time: 08:16
+ */
+
+namespace Modules\Routes;
+
+
+use Modules\Basic\BasicModule;
+
+class Landmarks extends BasicModule
+{
+    public function post($input) {
+        $res = [];
+
+        $q = "INSERT INTO `routes_landmarks` (`route_ID`, `value`, `landmark_order`) VALUES (:route, :val, :land_order)";
+
+        $stmt = $this->db->prepare($q);
+
+        foreach ($input->data as $landmark) {
+            if ($stmt->execute([
+                ':route' => $landmark->route_ID,
+                ':val' => $landmark->value,
+                ':land_order' => $landmark->landmark_order
+            ])) {
+                $res[] = $this->db->lastInsertId();
+            }
+        }
+
+        return $res;
+    }
+
+    public function get($data)
+    {
+        $res = [];
+
+        $q = "SELECT ID, value, landmark_order FROM routes_landmarks";
+        $wheres = [];
+        $params = [];
+
+        if (property_exists($data, 'id') && !empty($data->id)) {
+            $wheres[] = 'ID = :id';
+            $params[':id'] = $data->id;
+        }
+
+        if (property_exists($data, 'route_ID') && !empty($data->route_ID)) {
+            $wheres[] = 'route_ID = :route';
+            $params[':route'] = $data->route_ID;
+        }
+
+        if (count($wheres) > 0) {
+            $q .= " WHERE " . implode(' AND ', $wheres);
+        }
+
+        $q .= " ORDER BY landmark_order ASC";
+
+        $stmt = $this->db->prepare($q);
+
+        if ($stmt->execute($params)) {
+            if ($landmarks = $stmt->fetchAll(\PDO::FETCH_OBJ)) {
+                $res = $landmarks;
+            }
+        }
+
+        return $res;
+    }
+}
--- static/src/js/services/src/routes.js
+++ static/src/js/services/src/routes.js
@@ -19,6 +19,20 @@ angular.module('services.routes', []).service('RoutesSvc', ['$http', '$q', 'url'
         return defer.promise;
     };
 
+    self.getUserRoutes = function (filters) {
+        var defer = $q.defer();
+
+        $http.get(url + 'users/routes/', {params: angular.extend({}, filters)}).then(function (res) {
+            if (res.data.status) {
+                defer.resolve(res.data.results);
+            } else {
+                defer.resolve([]);
+            }
+        });
+
+        return defer.promise;
+    };
+
     self.addRoute = function (route) {
         var defer = $q.defer();
 
