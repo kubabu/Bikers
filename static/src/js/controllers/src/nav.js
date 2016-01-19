06d6938d82829aa16632572d2d160d66c46da552
Kuba Buda <kubabuda@gazeta.pl>
2016-01-19 22:23:42 +0100

    Display nread message count

--- static/src/js/controllers/src/nav.js
+++ static/src/js/controllers/src/nav.js
@@ -1,4 +1,5 @@
 angular.module('controllers.nav', []).controller('NavCtrl', ['$scope', 'AuthSvc', function ($scope, AuthSvc) {
+    $scope.unread_msg_count = 0;
     $scope.urls = [
         {path: '#/users/', name: 'Tablica', active: false},
         {path: '#/bikes/', name: 'Rowery', active: false},
